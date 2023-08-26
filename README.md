fd7a7bf8bd75e4840f995dd4569fa6b28c343e4d
Abdulaziz Komilov <89730020+sulton0011@users.noreply.github.com>
2023-08-26 14:22:20 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# errs
-Error struct
+# Error Wrapping Utility [![AppVeyor](https://ci.appveyor.com/api/projects/status/b98mptawhudj53ep/branch/master?svg=true)](https://ci.appveyor.com/project/davecheney/errors/branch/master) [![GoDoc](https://godoc.org/github.com/pkg/errors?status.svg)](http://godoc.org/github.com/pkg/errors) [![Report card](https://goreportcard.com/badge/github.com/pkg/errors)](https://goreportcard.com/report/github.com/pkg/errors)
+
+### Package errors provides simple error handling primitives.
+
+```
+go get github.com/pkg/errors
+```
+
+This utility provides two functions, `Wrap` and `WrapLog`, designed to enhance error handling and logging in Go applications.
+
+### Function `Wrap`
+
+The `Wrap` function is used to wrap an existing error with additional messages. It facilitates creating more informative error messages by combining custom messages with the original error message. The resulting error provides improved context and helps with error tracing.
+
+### Usage
+
+```go
+newErr := Wrap(&originalErr, "Additional message 1", "Additional message 2")
+```
+
+If `originalErr` is not nil, `Wrap` will create a new error that includes the provided additional messages along with the original error message.
+
+## Function `WrapLog`
+
+The `WrapLog` function combines error wrapping with logging, making it useful for debugging and monitoring purposes. It logs the wrapped error along with request-related information and custom messages.
+
+### Usage
+
+```go
+WrapLog(&originalErr, requestInfo, "Additional log message")
+```
+
+If `originalErr` is not nil, `WrapLog` will log a message that combines the additional log message, request information, and the original error message.
+
+## Benefits
+
+- **Enhanced Error Information**: By using `Wrap` and `WrapLog`, you can easily add contextual information to error messages, making it easier to identify the source of errors.
+- **Simplified Debugging**: The functions simplify debugging by providing detailed error messages and logging information. This can significantly reduce the time it takes to identify and fix issues.
+- **Effective Monitoring**: With `WrapLog`, you can create informative log entries for error situations, aiding in real-time monitoring and issue detection.
+- **Readable and Maintainable Code**: By encapsulating error wrapping and logging logic, your codebase remains cleaner and easier to maintain.
+
+## Caution
+
+- Be mindful when using these functions. Overuse of excessive messages might clutter logs and make debugging more difficult.
+- Always use relevant, concise, and helpful messages to ensure that the combined error messages provide meaningful context.
+
+## Example
+
+Here's an example demonstrating the use of the `Wrap` and `WrapLog` functions:
+
+```go
+func (s *todoService) Get(ctx context.Context, req *pb.TodoRequest) (resp *pb.TodoResponse, err error) {
+	defer errors.WrapLog(&err, req, "todoService", "Get")
+
+	if _, err = strconv.Atoi(req.Discount); err != nil {
+		return nil, errors.Wrap(&err, "strconv.Atoi", 1)
+	}
+	if _, err = strconv.Atoi(req.Amount); err != nil {
+		return nil, errors.Wrap(&err, "strconv.Atoi", 2)
+	}
+
+	resp, err = s.Repo.Dodo.Get(ctx, req)
+
+	return 
+}
+```
+
+In this example, `WrapLog` logs the error with additional information, while `Wrap` combines the error with a custom error message for better error handling.
+
+### Console if there is an error
+
+```
+023/08/24 15:25:26 todoService--->Get request: { 3} Error: strconv.Atoi--->1--->strconv.Atoi: parsing "": invalid syntax
+```
---
+++ wrap.go
@@ -0,0 +1,128 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+// Unwrap returns the result of calling the Unwrap method on err, if err's
+// type contains an Unwrap method returning error.
+// Otherwise, Unwrap returns nil.
+//
+// Unwrap returns nil if the Unwrap method returns []error.
+func Unwrap(err Error) error {
+	u, ok := err.ErrorRes.(interface {
+		Unwrap() error
+	})
+	if !ok {
+		return nil
+	}
+	return u.Unwrap()
+}
+
+// Is reports whether any error in err's tree matches target.
+//
+// The tree consists of err itself, followed by the errors obtained by repeatedly
+// calling Unwrap. When err wraps multiple errors, Is examines err followed by a
+// depth-first traversal of its children.
+//
+// An error is considered to match a target if it is equal to that target or if
+// it implements a method Is(error) bool such that Is(target) returns true.
+//
+// An error type might provide an Is method so it can be treated as equivalent
+// to an existing error. For example, if MyError defines
+//
+//	func (m MyError) Is(target error) bool { return target == fs.ErrExist }
+//
+// then Is(MyError{}, fs.ErrExist) returns true. See syscall.Errno.Is for
+// an example in the standard library. An Is method should only shallowly
+// compare err and the target and not call Unwrap on either.
+func Is(err, target Error) bool {
+	return errors.Is(err.ErrorRes, target.ErrorRes)
+}
+
+// As finds the first error in err's tree that matches target, and if one is found, sets
+// target to that error value and returns true. Otherwise, it returns false.
+//
+// The tree consists of err itself, followed by the errors obtained by repeatedly
+// calling Unwrap. When err wraps multiple errors, As examines err followed by a
+// depth-first traversal of its children.
+//
+// An error matches target if the error's concrete value is assignable to the value
+// pointed to by target, or if the error has a method As(interface{}) bool such that
+// As(target) returns true. In the latter case, the As method is responsible for
+// setting target.
+//
+// An error type might provide an As method so it can be treated as if it were a
+// different error type.
+//
+// As panics if target is not a non-nil pointer to either a type that implements
+// error, or to any interface type.
+func As(err Error, target any) bool {
+	return errors.As(err.ErrorRes, target)
+}
+
+// Wrap wraps an error with additional messages.
+//
+// If the given error err is not nil, Wrap creates a new error by combining
+// messages from the msgs arguments with the text of the original error.
+// It returns a new error with the combined data.
+// If err is nil, the function returns nil.
+func Wrap(err *Error, msgs ...interface{}) Error {
+	if err.ErrorLog == nil {
+		return *err
+	}
+
+	var message string
+	for _, msg := range msgs {
+		if msg != nil {
+			if message != "" {
+				message += "--->"
+			}
+			message += fmt.Sprint(msg)
+		}
+	}
+
+	if message != "" {
+		err.ErrorLog = New(message + "--->" + (err.ErrorLog).Error())
+	}
+
+	if err.ErrorRes == nil {
+		err.ErrorRes = err.ErrorLog
+	}
+
+	return *err
+}
+
+// WrapLog wraps an error with additional messages and performs logging.
+//
+// If the given error err is not nil, WrapLog creates a message by combining
+// messages from the msgs arguments with the text of the original error.
+// It then logs this message along with information about the request and error.
+// If err is nil, the function does not take any action.
+func WrapLog(err *error, req interface{}, msgs ...interface{}) {
+	if *err == nil {
+		return
+	}
+
+	var message string
+	for _, msg := range msgs {
+		if msg != nil {
+			if message != "" {
+				message += "--->"
+			}
+			message += fmt.Sprint(msg)
+		}
+	}
+
+	log.Print(
+		message,
+		" request: ", req,
+		" Error: ", *err,
+	)
+}
